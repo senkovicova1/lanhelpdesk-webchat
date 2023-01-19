7b347ff75a935242fde0fd1801c9f76562a3869d
Senkovičová Soňa <sona.senkovicova@gmail.com>
2023-01-19 11:22:05 +0100

    S

--- src/configs/restAPI.js
+++ src/configs/restAPI.js
@@ -1,12 +1,8 @@
-import jwtDecode from 'jwt-decode';
-
-export const testing = true;
+export const testing = false;
 export const testingTranslations = testing;
 
-const tokenEncoded = window.location.href.substring(window.location.href.indexOf("token=") + 6);
-const port = jwtDecode(tokenEncoded).port;
-const URL = "lantask.eu"; //window.location.hostname;
-
+const port = window.location.port;
+const URL = window.location.hostname;
 //https://nodejs02.lanhelpdesk.com
 //173.212.231.78
 const protectedREST = `https://${URL}:${port}`;
@@ -15,4 +11,6 @@ const protectedSocket = `wss://${URL}:${port}`;
 const localSocket = `ws://localhost:4000`;
 
 export const REST_URL = testing ? localREST : protectedREST;
-export const SOCKET_URL = testing ? localSocket : protectedSocket;
+export const SOCKET_URL = testing
+    ? localSocket
+    : protectedSocket;
